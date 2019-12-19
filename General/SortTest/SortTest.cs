e91016ed02b05955b7022a7f46afaeaae1f80dac
Carlos Dias <csdias@gmail.com>
2019-12-19 12:30:38 -0300

    refactoring

---
+++ General/SortTest/SortTest.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using Sort;
+
+namespace SortTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
