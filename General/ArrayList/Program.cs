17a3b06cc44d1bd71db24071a7bce400c37260cf
Carlos Dias <csdias@gmail.com>
2019-12-19 20:03:08 -0300

    refactoring

---
+++ General/ArrayList/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+namespace ArrayList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            ArrayList<int> list = new ArrayList<int>();
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            list.Insert(200, 2);
+        }
+    }
+
+    public class ArrayList<T>{
+
+        T[] internalStorage;
+        int lastItemIndex;
+
+        public ArrayList()
+        {
+            internalStorage = new T[4];
+            lastItemIndex = -1;
+        }
+
+        public void Add(T item){
+
+            if (lastItemIndex == internalStorage.Length - 1)
+                ExpandStorage();
+
+            internalStorage[lastItemIndex + 1] = item;
+            lastItemIndex++;
+        }
+
+        public void Insert(T item, int index){
+            Validate(index);
+
+            if (lastItemIndex == internalStorage.Length - 1)
+                ExpandStorage();
+
+            int segment = lastItemIndex - index;
+
+            Array.Copy(internalStorage, index, internalStorage, index + 1, segment);
+
+            internalStorage[index] = item;
+
+            lastItemIndex++;
+        }
+
+        private void Validate(int index){
+            if (index == -1 || index > lastItemIndex)
+                throw new ArgumentOutOfRangeException();
+        }
+
+        private void ExpandStorage(){
+            T[] newStorage = new T[internalStorage.Length * 2];
+            Array.Copy(internalStorage, newStorage, internalStorage.Length - 1);
+
+            internalStorage = newStorage;
+        }
+
+        public T this[int index] { 
+            get { return internalStorage[index]; }
+            set { internalStorage[index] = value; }
+        }
+
+        public int Length { get { return internalStorage.Length;} }
+    
+    }
+}
---
+++ General/ArrayListTest/ArrayListTest.cs
@@ -0,0 +1,78 @@
+using NUnit.Framework;
+using ArrayList;
+using System;
+
+namespace ArrayListTest
+{
+    public class ArrayListTest
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ExpandedToFour()
+        {
+            //Assert.Pass();
+
+            ArrayList<int> list = new ArrayList<int>();
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+
+            Assert.AreEqual(4, list.Length);
+        }
+
+        [Test]
+        public void ExpandedToEight()
+        {
+            //Assert.Pass();
+
+            ArrayList<int> list = new ArrayList<int>();
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            list.Add(7);
+            list.Add(8);
+
+            Assert.AreEqual(8, list.Length);
+        }
+
+        public void ExpandedToEightAfterInsertion()
+        {
+            //Assert.Pass();
+
+            ArrayList<int> list = new ArrayList<int>();
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            list.Add(7);
+
+            //Assert.AreEqual(200, list.Length);
+            list.Insert(2, 200);
+
+            //Assert.AreEqual(8, list.Length);
+            //Assert.AreEqual(6, list[2]);
+            Assert.AreEqual(6, list[3]);
+        }
+
+        private void ThrowIndexOutOfRange_prep(){
+
+            ArrayList<int> list = new ArrayList<int>();
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            list[5] = 7;            
+        }
+
+        [Test]
+        public void ThrowIndexOutOfRangeException()
+        {
+            var ex = Assert.Throws<IndexOutOfRangeException>(() => { ThrowIndexOutOfRange_prep(); });
+            Assert.That(ex.Message, Is.EqualTo("Index was outside the bounds of the array."));
+        }        
+
+
+    }
+}
