10c5e1de20562a032d3d49fcc51621d897b87a87
Carlos Soares Dias <csdias@gmail.com>
2019-12-22 15:00:05 -0300

    super refactoring

---
+++ Algorithm/Sorting/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = "The number to be parsed is: 123";
+            string msg = "hello";
+
+            char[] charMsg = msg.ToCharArray(); 
+
+            charMsg[0] = 'Z';
+
+            //this does not create a new string
+            ReadOnlySpan<char> msgSpan = message;
+            ReadOnlySpan<char> numSpan = msgSpan.Slice(message.IndexOf(':')+2);
+            int.TryParse(numSpan, out int b);
+            Console.WriteLine(b);
+
+            int[] unsortedI  = { 800, 11, 50, 771, 649, 770, 240, 9 };
+            int[] unsortedII = new int[10] {2, 0, 1, 5, 7, 4, 9, 3, 8, 6};
+            int[] unsortedIII = {6, 42, 10, 86, 4, 12, 30, 23};
+
+            List<int> sorted = Sorting.MergeSort(unsortedIII.ToList<int>());
+            
+            foreach(var i in sorted){
+                Console.WriteLine(i);
+            }
+
+            //int[] sorted = BubbleSort(seq);
+
+            //int[] sorted = CocktailSort(seq);
+
+            //int s = BinarySearch(sorted, 9);
+            //int ss = NovoBinarySearch(sorted, 50);
+            //int sss. = RecursiveBinarySearch(sorted, 50);
+
+            //Console.WriteLine(BinarySearch(sorted, int x))
+
+
+        }
+
+    }
+}
---
+++ Algorithm/__tests__/ArrayListTest.cs
@@ -0,0 +1,76 @@
+using NUnit.Framework;
+using System;
+using ArrayList;
+
+namespace __tests__
+{
+    public class Tests
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ExpandedToFour()
+        {
+            //Assert.Pass();
+
+            ArrayList<int> list = new ArrayList<int>();
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+
+            Assert.AreEqual(4, list.Length);
+        }
+
+        [Test]
+        public void ExpandedToEight()
+        {
+            //Assert.Pass();
+
+            ArrayList<int> list = new ArrayList<int>();
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            list.Add(7);
+            list.Add(8);
+
+            Assert.AreEqual(8, list.Length);
+        }
+
+        [Test]
+        public void ExpandedToEightAfterInsertion()
+        {
+            ArrayList<int> list = new ArrayList<int>();
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            list.Add(7);
+            list.Add(8);            
+
+            list.Insert(200, 2);
+            
+            Assert.AreEqual(200, list[2]);
+            Assert.AreEqual(6, list[3]);
+        }
+
+        private void ThrowIndexOutOfRange_prep(){
+
+            ArrayList<int> list = new ArrayList<int>();
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            list[5] = 7;            
+        }
+
+        [Test]
+        public void ThrowIndexOutOfRangeException()
+        {
+            var ex = Assert.Throws<IndexOutOfRangeException>(() => { ThrowIndexOutOfRange_prep(); });
+            Assert.That(ex.Message, Is.EqualTo("Index was outside the bounds of the array."));
+        }       
+
+    }
+}
