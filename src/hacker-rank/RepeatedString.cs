5004997cbeeaf5a2b0101fcb05b62329b10c7b53
Carlos Soares Dias <csdias@gmail.com>
2020-04-18 18:14:50 -0300

    Adding HackerRank challenges

---
+++ src/hacker-rank/RepeatedString.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace hackerRank
+{
+    public static class RepeatedString
+    {
+
+        static void Main(string[] args)
+        {
+            var result = repeatedString("aba", 10);
+
+            Console.WriteLine(result);
+        }
+
+        // Complete the repeatedString function below.
+        static long repeatedString(string s, long n)
+        {
+            long size = s.Length;
+            long ass = 0;
+            for (var x = 0; x <= size - 1; x++)
+            {
+                if (s[x] == 'a')
+                    ass++;
+            }
+
+            long remaining = n % size;
+
+            ass = (n / size) * ass;
+
+            for (var y = 0; y <= remaining; y++)
+            {
+                if (s[y] == 'a')
+                    ass++;
+
+            }
+            return ass;
+        }
+    }
+
+
+}
--- src/search/BinarySearch.cs
+++ src/search/BinarySearch.cs
@@ -1,21 +1,37 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Dynamic;
 
 namespace Algorithm
 {
     public partial class Search
     {
+        
         public static void Main(string[] args){
 
-            int[] res = BinarySearch(new int[]{1,2,3});
 
         }
 
-        public static int[] BinarySearch(int[] input){
+        static int BinarySearch(int[] toBeSearched, int item)
+        {
+            int min = 0;
+            int max = toBeSearched.Length -1;
+            while(min <= max){
+                int mid = (max + min)/2;
 
+                if (item > toBeSearched[mid]){
+                    min = mid +1;
+                }
+                else if (item < toBeSearched[mid]){
+                    max = mid -1;
+                }
+                else if(item == toBeSearched[mid])
+                {
+                    return ++mid;
+                }
+            }
 
-            return new int[]{1,2};
+            return -1;
         }
-
     }
 }
